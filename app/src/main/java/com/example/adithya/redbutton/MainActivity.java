e74fa2c6ba5b7edee062c49653784259c13ffff1
Wen <leonardhsin@gmail.com>
2016-11-12 22:58:35 -0500

    send text feature and preserve across different runs feature.

--- app/src/main/java/com/example/adithya/redbutton/MainActivity.java
+++ app/src/main/java/com/example/adithya/redbutton/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.adithya.redbutton;
 
+import android.Manifest;
+import android.content.SharedPreferences;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickRekt(View v) {
+        SharedPreferences sp = getSharedPreferences("rekt", 0);
+        SharedPreferences.Editor spe = sp.edit();
+        spe.putString("slogan", "Adithya is rekt.").commit();
+        System.out.println("onClickRekt being called.");
+    }
+
+    public void onClickUnrekt(View v) {
+        SharedPreferences sp = getSharedPreferences("rekt", 0);
+        String toastString = sp.getString("slogan", "Nothing named slogan was stored");
+        Toast testToast = Toast.makeText(getApplicationContext(), toastString, Toast.LENGTH_LONG);
+        testToast.show();
+    }
+
+    public void sendRekt(View v) {
+        /* Ask user for permission to send SMS */
+        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, 1);
+        /* TODO: check if user actually grant the permission */
+        SmsManager smsManager = SmsManager.getDefault();
+        String wensPhoneNumber = "7328613900";
+        String text = "Let us unrekt Adithya\n";
+        smsManager.sendTextMessage(wensPhoneNumber, null, text, null, null);
     }
 
 
